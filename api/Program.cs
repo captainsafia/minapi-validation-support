a80163e5a3f7ab6974cfbad995936e073e1bd4ca
Safia Abdalla <safia@safia.rocks>
2025-08-12 11:01:07 -0700

    Clean up code comment

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,5 @@
 #pragma warning disable ASP0029
 
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Text.Json.Serialization;
